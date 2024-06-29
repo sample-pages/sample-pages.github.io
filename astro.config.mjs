b6725375259c92b2f1e6b24e3ea782ab5befe6d0
luetsrr <ymstar4160@gmail.com>
2024-06-30 03:28:26 +0900

    todai-math-2024

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://luetsrr.com'
+    site: 'https://luetsrr.com',
+    integrations: [mdx()],
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+      },
 });
   
