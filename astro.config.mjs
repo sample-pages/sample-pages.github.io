5d2032ddac08355d6e03bb6554d794e07fe40c14
Yuma Ikeda <yumaikeda@Yumas-MacBook-Air.local>
2025-12-15 15:39:55 +0900

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://luetsrr.github.io/',
+    site: 'https://sample-pages.github.io/',
     integrations: [mdx()],
     markdown: {
         remarkPlugins: [remarkMath],
